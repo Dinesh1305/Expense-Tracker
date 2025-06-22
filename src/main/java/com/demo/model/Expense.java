c0c3926ea330528dc3323ee5bfc95b94546a7e94
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-06-22 20:06:49 +0530

    The Expense model was created

---
+++ src/main/java/com/demo/model/Expense.java
@@ -0,0 +1,34 @@
+package com.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Expense {
+
+	@Id
+	private Integer id;
+	
+	@Column(nullable=false)
+	private String category;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+	
+}
